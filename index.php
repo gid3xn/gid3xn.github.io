da1954315e3bf0ed754a7c747b4613949bfd0f26
Gideon <96313759+gid3xn@users.noreply.github.com>
2023-07-05 10:31:27 -0700

    a bunch of changes

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+    </head>
+    <body>
+        <?php include 'navbar.php'; ?>
+
+        <!-- Main content -->
+        <div class="main-content">
+            <h1>Gid3xn's Website</h1>
+            <p> I am Gid3xn. Welcome to my Web Site.</p>
+            <p> 
+                <ul>
+                    <li><a href="writing.html">Writing</a></li>
+                </ul>
+            </p>
+            <p>I am a trial lawyer building this website from scratch, teaching myself CSS, HTML, and Git along the way.</p>
+        </div>
+    </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,7 @@
+<!-- navbar.php -->
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="writing.php">Writing</a></li>
+    </ul>
+</nav>
